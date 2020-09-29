23742e3d724ad38c0df0361bb78af3273408fe4d
jameswilles <james.m.willes@gmail.com>
2020-09-29 15:13:37 -0600

    Final Push

--- Solutions.sql
+++ Solutions.sql
@@ -100,3 +100,83 @@
 --6
 -- SELECT SUM(product_price * quantity) FROM orders
 -- WHERE person_id = 0;
+
+
+-- Table – artist
+
+--1
+-- INSERT INTO artist (name)
+-- VALUES
+-- ('Katie'),
+-- ('David'),
+-- ('Jordan');
+
+--2
+-- SELECT * FROM artist
+-- ORDER BY name DESC
+-- LIMIT 10;
+
+--3
+-- SELECT * FROM artist
+-- ORDER BY name ASC
+-- LIMIT 5;
+
+--4
+-- SELECT * FROM artist
+-- WHERE name LIKE 'Black%';
+
+--5
+-- SELECT * FROM artist
+-- WHERE name LIKE '%Black%';
+
+
+-- Table – employee
+
+--1
+-- SELECT first_name, last_name FROM employee
+-- WHERE city = 'Calgary';
+
+--2
+-- SELECT MAX(birth_date) FROM employee;
+
+--3
+-- SELECT MIN(birth_date) FROM employee;
+
+--4
+-- SELECT * FROM employee
+-- WHERE reports_to = 2;
+
+--5
+-- SELECT COUNT(*) FROM employee
+-- WHERE city = 'Lethbridge';
+
+
+-- Table – invoice
+
+--1
+-- SELECT COUNT(*) FROM invoice WHERE billing_country = 'USA';
+
+--2 
+-- SELECT MAX(total) FROM invoice;
+
+--3
+-- SELECT MIN(total) FROM invoice;
+
+--4
+-- SELECT * FROM invoice
+-- WHERE total > 5;
+
+--5
+-- SELECT COUNT(*) FROM invoice
+-- WHERE total < 5;
+
+--6
+-- SELECT COUNT(*) FROM invoice
+-- WHERE billing_state in ('CA', 'TX', 'AZ');
+
+--7
+-- SELECT AVG(total) FROM invoice;
+
+--8
+-- SELECT SUM(total) FROM invoice;
+
